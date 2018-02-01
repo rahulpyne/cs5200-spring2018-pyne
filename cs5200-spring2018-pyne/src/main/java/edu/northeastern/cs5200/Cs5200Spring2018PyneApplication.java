b2622c9512c66d0e4387a3387d7ac3289d096c5e
Rahul Pyne <rahulpyne@ccs.neu.edu>
2018-02-01 03:35:39 -0500

    Integrating Spring Boot with MySQL

--- cs5200-spring2018-pyne/src/main/java/edu/northeastern/cs5200/Cs5200Spring2018PyneApplication.java
+++ cs5200-spring2018-pyne/src/main/java/edu/northeastern/cs5200/Cs5200Spring2018PyneApplication.java
@@ -2,10 +2,16 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Spring2018PyneApplication {
+public class Cs5200Spring2018PyneApplication extends SpringBootServletInitializer {
 
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(Cs5200Spring2018PyneApplication.class);
+	 }
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Spring2018PyneApplication.class, args);
 	}
